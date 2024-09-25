e99e646b27de134a9ed8817110a5f8e791ca1175
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-09-25 08:58:29 -0700

    done'

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ console.log(
 );
 
 // Exercise 1
-
-// Exercise 2
+const firstName = "Adedapo";
+let lastName = "Aluko";
+var age = 33;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} was released in ${createdYear}.`);
+
+let product = "Sweat pant";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`${product} in size ${selectedSize} costs $${price} with ${stock} in stock.`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(`${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}.`);
